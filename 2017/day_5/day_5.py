aac60478ed5efd6a80cc9a2f103a606fe74a78ac
ebehlmann <ebehlmann@gmail.com>
2017-12-05 22:13:42 -0600

    adding part 2 solution

--- 2017/day_5/day_5.py
+++ 2017/day_5/day_5.py
@@ -1,6 +1,3 @@
-with open ('input.txt') as input_file:
-	puzzle_input = input_file.readlines()
-
 def clean_up_input(puzzle_input):
 	i = 0
 	while(i < len(puzzle_input)):
@@ -8,16 +5,37 @@ def clean_up_input(puzzle_input):
 		i += 1
 	return puzzle_input
 
-def step_through_instructions(puzzle_input):
+def step_through_instructions(puzzle_input, part):
 	i = 0
 	steps = 0
 
 	while(i < len(puzzle_input)):
 		i_value = puzzle_input[i]
-		puzzle_input[i] += 1
+		if(part == 1):
+			puzzle_input[i] += 1
+		else:
+			if(puzzle_input[i] >= 3):
+				puzzle_input[i] -= 1
+			else: 
+				puzzle_input[i] += 1
 		i += i_value
 		steps += 1
 
 	return steps
 
-print(step_through_instructions(clean_up_input(puzzle_input)))
+with open ('input.txt') as input_file:
+	puzzle_input = input_file.readlines()
+
+cleaned_input = clean_up_input(puzzle_input)
+
+print('Part 1:')
+print(step_through_instructions(cleaned_input, 1))
+
+# reset everything 
+with open ('input.txt') as input_file:
+	puzzle_input = input_file.readlines()
+
+cleaned_input = clean_up_input(puzzle_input)
+
+print('Part 2:')
+print(step_through_instructions(cleaned_input, 2))
